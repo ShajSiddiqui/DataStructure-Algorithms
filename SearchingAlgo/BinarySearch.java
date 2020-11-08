c4151bdb8ada98782111a18b3e95a016151828d9
shajoffcl <shaj.native@gmail.com>
2020-11-08 23:10:11 +0530

    searching added

---
+++ SearchingAlgo/BinarySearch.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class BinarySearch 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Declare length:");
+		int len=sc.nextInt();
+
+		int arr[]=new int[len];
+
+		System.out.println("Enter items:");
+		for(int i=0;i<len;i++){
+			arr[i]=sc.nextInt();
+		}
+
+		System.out.println("Enter search item:");
+		int searchEl=sc.nextInt();
+
+		int index=binarySearch(arr, len, searchEl);
+
+		if(index==-1){
+			System.out.println("OOPS! Item not found.");
+		}else{
+			System.out.println("Item found at index "+index);
+		}
+	}
+
+	private static int binarySearch(int[] arr, int len, int search){
+		int start=0;
+		int end=len-1;
+		while(start<=end){
+			int mid=(start+end)/2;
+			
+			if(arr[mid]<search){
+				start=mid+1;
+			}
+			else if(arr[mid]==search){
+				return mid;
+			}
+			else{
+				end=mid-1;
+			}
+		}
+		return -1;
+	}
+}
